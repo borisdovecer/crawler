1d27842d54bb5dbd5ecdf6bd9647ee353fd73f20
borisdovecer <36776986+borisdovecer@users.noreply.github.com>
2021-03-27 00:40:32 +0100

    v1.01

--- puppeteer.js
+++ puppeteer.js
@@ -9,25 +9,24 @@ async function getData(p) {
     const page = await browser.newPage();
     await page.goto(`https://www.shop-apotheke.com/homoeopathisch-hautpflege/?pageNumber=${p}`);
 
-    const title = await page.evaluate(() => {
-        return document.querySelector(".u-margin--bottom:nth-child(1)").innerText;
-    });
-
-    const price = await page.evaluate(() => {
-        return document.querySelector(".o-ProductPrice:last-child > .u-display--inline-block:last-child").innerText;
-    });
-
-    const pzn = await page.evaluate(() => {
-        return document.querySelector(".o-ProductPackageDetails > div:nth-child(2)").innerText;
-    });
-
-    const img = await page.evaluate(() => {
-        return document.querySelector(".o-ProductImage > .m-ImageWithDiscountBadge > picture > img").src;
+    const data = await page.evaluate(() => {
+        let arr = []
+        for (let i = 1; i<33;i++) {
+            let x = document.querySelector(`ul.o-Hits > li:nth-child(${i}) `)
+            if(x.innerText !== ""){
+                let title = document.querySelector(`li:nth-child(${i}) > .l-flex > .o-FilteredProductListItem__content > .l-flex--stretch-children > div:nth-child(1) > .u-margin--bottom:nth-child(1)`).innerText
+                let price = document.querySelector(`li:nth-child(${i}) > .l-flex > .o-FilteredProductListItem__content > .l-flex--stretch-children > div:nth-child(2) > .u-margin--bottom:nth-child(1) > .o-ProductPriceInfo > .u-margin-small--bottom > .u-display--inline-block > .o-ProductPrice > div:nth-child(3)`).innerText
+                let pzn = document.querySelector(`li:nth-child(${i}) > .l-flex > .o-FilteredProductListItem__content > .l-flex--stretch-children > div:nth-child(1) > div:nth-child(4) > .o-ProductPackageDetails > div:nth-child(2)`).innerText
+                let image = document.querySelector(`li:nth-child(${i}) > .l-flex > .o-FilteredProductListItem__image > .a-Button > .o-ProductImage > .m-ImageWithDiscountBadge > picture > img`).src
+                arr.push({title,price,pzn, image})
+            }
+        }
+        return arr
     });
 
     await browser.close();
 
-   return {title, price, pzn, img};
+    return {data};
 }
 
 app.get('/', (req, res) => {
@@ -36,6 +35,8 @@ app.get('/', (req, res) => {
     data.then(function(result) {
         console.log(result)
         res.json(result)
+    }).catch(function (err){
+        console.log(err)
     })
 })
 
